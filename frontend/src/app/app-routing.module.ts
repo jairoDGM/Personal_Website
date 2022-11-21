78103921156da50bb0b2c1f8c785b7aefe62d747
jairoDGM <jairo.danielgm@gmail.com>
2022-11-21 12:23:37 -0600

    definicion de rutas basicas para blog y services

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './componentes/about-me/about-me.component';
+import { BlogComponent } from './componentes/blog/blog.component';
+import { ServicesComponent } from './componentes/services/services.component';
 
 const routes: Routes = [
-  {path:"",component:AboutMeComponent}
+  {path:"",component:AboutMeComponent},
+  {path:"blog",component:BlogComponent},
+  {path:"services",component:ServicesComponent},
 ];
 
 @NgModule({
